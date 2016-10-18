f614ee6395a1f706228dc64345b2c96604a737bd
Aphid <aphid@youkai.com>
2016-10-19 06:17:32 +1100

    Completed module3-solution.

---
+++ module3-solution/app.js
@@ -0,0 +1,89 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'E',
+    templateUrl: 'founditems.html',
+    scope: {
+      list: '<foundItems',
+      title: '@title',
+      remove: '&onRemove'
+    }
+  };
+  return ddo;
+};
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var self = this;
+
+  var baseTitle = "Found Items";
+
+  self.title = baseTitle;
+  self.searchTerm = "";
+  self.found = [];
+
+  self.searchMenu = function() {
+      self.found = [];
+      var matchedMenuItemsPromise = MenuSearchService.getMatchedMenuItems(this.searchTerm);
+
+      matchedMenuItemsPromise.then(function (response) {
+        self.found = response;
+        self.title = baseTitle + " (" + self.found.length + ")";
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+
+  self.removeFoundItem = function(itemIndex) {
+    self.found.splice(itemIndex, 1);
+    self.title = baseTitle + " (" + self.found.length + ")";
+  }
+};
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+  var menuItemList = [];
+
+  service.getMatchedMenuItems = function(searchTerm) {
+      menuItemList = [];
+
+      return $http({method: "GET", url: (ApiBasePath + "/menu_items.json")})
+      .then(function (result) {
+
+        // process result and only keep items that match
+        var responseData = result.data.menu_items;
+        console.log(responseData.length);
+        for (var i=0; i<responseData.length; i++) {
+          var item = responseData[i];
+          var name = item.name;
+          var description = item.description;
+
+          if (IsStringInString(searchTerm, description)) {
+            menuItemList.push(item);
+          };
+        };
+
+        // return processed items
+        return menuItemList;
+      });
+  };
+};
+
+function IsStringInString(searchString, searchInString) {
+  if (searchInString.toLowerCase().match(searchString.toLowerCase())) {
+    return true;
+  }
+  return false;
+};
+
+})();
