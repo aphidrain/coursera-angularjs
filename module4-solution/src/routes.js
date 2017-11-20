b43cba3911abf631d8f50d06f35e373777da5896
mishoryu <22555149+mishoryu@users.noreply.github.com>
2017-11-20 18:12:01 +1100

    Minor style changes.

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -19,25 +19,25 @@
       .state('categories', {
         url: '/categories',
         templateUrl: 'src/categories.template.html',
+        controller: 'CategoriesController as cats_ctrl',
         resolve: {
           categories: ['MenuDataService', function(MenuDataService) {
             return MenuDataService.getAllCategories();
           }]
-        },
-        controller: 'CategoriesController as ctrl'
+        }
       })
 
       // spa items view
       .state('items', {
         url: '/items/{itemCategory}',
         templateUrl: 'src/items.template.html',
+        controller: 'ItemsController as items_ctrl',
         resolve: {
           items: ['MenuDataService', '$stateParams',
             function (MenuDataService, $stateParams) {
               return MenuDataService.getItemsForCategory($stateParams.itemCategory);
             }]
-        },
-        controller: 'ItemsController as ctrl'
+        }
       });
   }
 
